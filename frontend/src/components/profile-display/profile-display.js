e40efd76dcf58a035c6d00b63953ef46dc898759
Victor Navarro <vn4varro@gmail.com>
2021-02-05 21:16:29 +0100

    feat: profile display component

---
+++ frontend/src/components/profile-display/profile-display.js
@@ -0,0 +1,26 @@
+import profileImage from "../../images/profile-placeholder.jpg";
+import './profile-display.css'
+import useAuth from "../../hooks/use-auth";
+import {useHistory} from "react-router-dom";
+
+const ProfileDisplay = () => {
+    const history = useHistory()
+    const {logout} = useAuth()
+
+    const handleLogout = () => {
+        logout()
+        history.push("/login")
+    }
+
+    return (
+        <section className="profile-display-container">
+            <img className="profile-display-image" src={profileImage} alt="profile image"/>
+            <div className="profile-display-content">
+                <h3 className="profile-display-name">Anonymous</h3>
+                <button className="profile-display-button" onClick={handleLogout}>Sign out</button>
+            </div>
+        </section>
+    )
+}
+
+export default ProfileDisplay
