94653f178e9b7329ed144a93f7640842e57ee5e5
Victor Navarro <vn4varro@gmail.com>
2021-02-08 18:52:23 +0100

    chore: use an env for port

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const authRouter = require('./routes/auth-router')
 const listRouter = require('./routes/list-router')
 
+const port = process.env.PORT || 3001;
+
 const app = express()
 
 app.use(cors())
@@ -12,10 +14,10 @@ app.use(express.json())
 app.use(authRouter)
 app.use(listRouter)
 
-app.listen(3001, () => {
+app.listen(port, () => {
     mongoose.connect('mongodb://localhost:3002/minimalist', {useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => console.log('Connected to MongoDB'))
         .catch(console.error)
 
-    console.log('Listening to http://localhost:3000');
+    console.log(`Listening to http://localhost:${port}`);
 })
