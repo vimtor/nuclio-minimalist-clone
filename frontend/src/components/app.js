f3865bbb6401d5c6bd57e748b1104503b30bfa5b
Pablo-Villanueva-Garcia <pablovillanuevagarcia88@gmail.com>
2021-02-17 19:57:41 +0100

    Redirect loggged

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,16 +3,22 @@ import RegisterPage from '../pages/register-page'
 import ListPage from '../pages/list-page/list-page'
 import AuthProvider from "../contexts/auth-context";
 import ProtectedRoute from "./protected-route/protected-route";
+import ProtectedLogged from "./protected-logged/protected-logged";
 import LoginPage from "../pages/login-page";
 import ListsProvider from "../contexts/lists-context";
+import { Redirect } from 'react-router-dom';
 
 const App = () => (
     <BrowserRouter>
         <AuthProvider>
 
             <Switch>
-                <Route path="/register" component={RegisterPage} />
-                <Route path="/login" component={LoginPage} />
+               
+                <ProtectedLogged Route path="/register" component={RegisterPage} />
+                <ProtectedLogged Route path="/login" component={LoginPage} />
+                <Route exact path="/">
+                    <Redirect to="/login"></Redirect>
+                </Route>
                 <ListsProvider>
                     <ProtectedRoute path="/lists" component={ListPage} />
                 </ListsProvider>
---
+++ frontend/src/components/protected-logged/protected-logged.js
@@ -0,0 +1,19 @@
+import {Route, Redirect} from "react-router-dom";
+import useAuth from "../../hooks/use-auth";
+
+const ProtectedLogged = ({component: Component, ...rest}) => {
+    const {logged} = useAuth();
+    return (
+        <Route
+            {...rest}
+            render={props => {
+                if (logged) {
+                    return <Redirect to="/lists" />
+                }
+                return <Component {...props} />
+            }}
+        />
+    )
+}
+
+export default ProtectedLogged;
