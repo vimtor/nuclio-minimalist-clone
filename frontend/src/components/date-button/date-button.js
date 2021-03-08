3cdf3c214b1dd7a1e08bfe47154ef26bda9f73cb
fmadueno <fmaduenoma@gmail.com>
2021-03-08 21:07:32 +0100

    feat: fix login block error when user login

--- frontend/src/components/date-button/date-button.js
+++ frontend/src/components/date-button/date-button.js
@@ -3,9 +3,7 @@ import { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import { registerLocale } from "react-datepicker";
 import es from "date-fns/locale/es";
-// import "react-datepicker/dist/react-datepicker.css";
 import "./date-button.css";
-// import styles from "./date-button.css";      //do not work. Must use normal .css file
 
 const DateButton = ({ date, updateDueDate }) => {
   const [dueDate, setDueDate] = useState(date ? new Date(date) : null);
